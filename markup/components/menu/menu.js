ab1b3500bcae123c28981e4ece866613b9c76f8e
SRG94 <song94.94@mail.ru>
2017-04-05 02:47:13 +0500

    05.04 night

--- markup/components/menu/menu.js
+++ markup/components/menu/menu.js
@@ -1,4 +1,11 @@
 $(document).on('click','.js-menu',function() {
 	$(this).toggleClass('_active');
 	$('.menu__list').slideToggle(300);
-})
+	$('.overlay').addClass('_active');
+});
+
+$(document).on('click','.overlay._active',function() {
+	$('.menu__list').hide();
+	$('.overlay').removeClass('_active');
+	$('.js-menu').removeClass('_active');
+});
